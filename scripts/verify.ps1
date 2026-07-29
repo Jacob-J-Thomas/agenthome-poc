5908a22dcdee9bfb30ecc8758d1087358cc51de4
Jacob-J-Thomas <92639671+Jacob-J-Thomas@users.noreply.github.com>
2026-07-29 12:25:40 -0500

    Make frontend verification self-contained

--- scripts/verify.ps1
+++ scripts/verify.ps1
@@ -62,6 +62,7 @@ try {
 
     $runningOnWindows = [System.Runtime.InteropServices.RuntimeInformation]::IsOSPlatform([System.Runtime.InteropServices.OSPlatform]::Windows)
     $npm = if ($runningOnWindows) { "npm.cmd" } else { "npm" }
+    Invoke-CheckedNative $npm @("ci")
     Invoke-CheckedNative $npm @("test")
 
     if ($RunBrowserE2E) {
